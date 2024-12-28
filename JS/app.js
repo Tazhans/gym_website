2435be34b1849121c70c9040b23509c0156171ee
Tazwell <taz.hans@gmail.com>
2024-12-28 13:14:04 +0200

    Removes animation  and opacity 0 from website

--- JS/app.js
+++ JS/app.js
@@ -9,102 +9,109 @@ $(document).ready(function() {
         })
       });
 
-      $(".man_working_out_animation").waypoint(function() {
-        $(".man_working_out_animation").addClass("animate__animated animate__fadeInLeft");
-      }, 
-        offset
-      );
+      // $(".man_working_out_animation").waypoint(function() {
+      //   $(".man_working_out_animation").addClass("animate__animated animate__fadeInLeft");
+      // }, 
+      //   offset
+      // );
+
+    //   $(".man_working_out_animation").waypoint(function() {
+    //     $(".man_working_out_animation").addClass("animate__animated animate__fadeInLeft");
+    // }, 
+    // {offset: "80%"}
+    // );
+    
 
     // Classes Section
-      $(".class_image").waypoint(function() {
-        $(".class_image").addClass("animate__animated animate__slideInUp");
-      }, 
-        offset
-      );
+      // $(".class_image").waypoint(function() {
+      //   $(".class_image").addClass("animate__animated animate__slideInUp");
+      // }, 
+      //   offset
+      // );
 
-      $(".class_image_2").waypoint(function() {
-        $(".class_image_2").addClass("animate__animated animate__slideInUp");
-      }, 
-        offset
-      );
+      // $(".class_image_2").waypoint(function() {
+      //   $(".class_image_2").addClass("animate__animated animate__slideInUp");
+      // }, 
+      //   offset
+      // );
 
-      $(".class_image_3").waypoint(function() {
-        $(".class_image_3").addClass("animate__animated animate__slideInUp");
-      }, 
-        offset
-      );
+      // $(".class_image_3").waypoint(function() {
+      //   $(".class_image_3").addClass("animate__animated animate__slideInUp");
+      // }, 
+      //   offset
+      // );
 
-      // End Classes Section
+      // // End Classes Section
 
-      // Team Members Section
+      // // Team Members Section
 
-      $(".team_members").waypoint(function() {
-        $(".team_members").addClass("animate__animated animate__zoomInRight animate__slow");
-      }, 
-        offset
-      );
+      // $(".team_members").waypoint(function() {
+      //   $(".team_members").addClass("animate__animated animate__zoomInRight animate__slow");
+      // }, 
+      //   {offset: "90%"}
+      // );
 
-      $(".team_members_2").waypoint(function() {
-        $(".team_members_2").addClass("animate__animated animate__zoomInUp animate__slow");
-      }, 
-        offset
-      );
+      // $(".team_members_2").waypoint(function() {
+      //   $(".team_members_2").addClass("animate__animated animate__zoomInUp animate__slow");
+      // }, 
+      //   {offset: "90%"}
+      // );
 
-      $(".team_members_3").waypoint(function() {
-        $(".team_members_3").addClass("animate__animated animate__zoomInLeft animate__slow");
-      }, 
-        offset
-      );
+      // $(".team_members_3").waypoint(function() {
+      //   $(".team_members_3").addClass("animate__animated animate__zoomInLeft animate__slow");
+      // }, 
+      //   {offset: "90%"}
+      // );
 
-      // End Team Members Section
+      // // End Team Members Section
 
-      // Gallery Section
+      // // Gallery Section
 
-      $(".gallery").waypoint(function() {
-        $(".gallery").addClass("animate__animated animate__slideInLeft animate__delay__2");
-      }, 
-        {offset: "80%"}
-      );
+      // $(".gallery").waypoint(function() {
+      //   $(".gallery").addClass("animate__animated animate__slideInLeft animate__delay__2");
+      // }, 
+      //   {offset: "80%"}
+      // );
 
-      $(".gallery_2").waypoint(function() {
-        $(".gallery_2").addClass("animate__animated animate__slideInUp");
-      }, 
-        offset
-      );
+      // $(".gallery_2").waypoint(function() {
+      //   $(".gallery_2").addClass("animate__animated animate__slideInUp");
+      // }, 
+      // {offset: "80%"}
+      // );
 
-      $(".gallery_3").waypoint(function() {
-        $(".gallery_3").addClass("animate__animated animate__slideInRight");
-      }, 
-        offset
-      );
+      // $(".gallery_3").waypoint(function() {
+      //   $(".gallery_3").addClass("animate__animated animate__slideInRight");
+      // }, 
+      // {offset: "80%"}
+      // );
 
-      $(".gallery_4").waypoint(function() {
-        $(".gallery_4").addClass("animate__animated animate__slideInLeft");
-      }, 
-        offset
-      );
+      // $(".gallery_4").waypoint(function() {
+      //   $(".gallery_4").addClass("animate__animated animate__slideInLeft");
+      // }, 
+      // {offset: "80%"}
+      // );
 
-      $(".gallery_5").waypoint(function() {
-        $(".gallery_5").addClass("animate__animated animate__slideInUp");
-      }, 
-        offset
-      );
+      // $(".gallery_5").waypoint(function() {
+      //   $(".gallery_5").addClass("animate__animated animate__slideInUp");
+      // }, 
+      // {offset: "80%"}
+      // );
 
-      $(".gallery_6").waypoint(function() {
-        $(".gallery_6").addClass("animate__animated animate__slideInRight");
-      }, 
-        offset
-      );
+      // $(".gallery_6").waypoint(function() {
+      //   $(".gallery_6").addClass("animate__animated animate__slideInRight");
+      // }, 
+      // {offset: "80%"}
+      // );
 
-      // End Gallery Section
+      // // End Gallery Section
 
-      // Contact Us Section
+      // // Contact Us Section
 
-      $(".contact_us_section").waypoint(function() {
-        $(".contact_us_section").addClass("animate__animated animate__zoomInUp animate__slow")
-      },
-      offset
-      );
+      // $(".contact_us_section").waypoint(function() {
+      //   $(".contact_us_section").addClass("animate__animated animate__zoomInUp animate__slow")
+      // },
+      // offset
+      // );
 
       // End Contact Us Section
 
